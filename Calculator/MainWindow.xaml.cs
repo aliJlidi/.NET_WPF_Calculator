d6dd6ed5f7630a1666445f9ca1d1c5303dd4b742
Ali <ali_jlidi85@yahoo.com>
2023-03-12 12:14:31 +0100

    finishing the app

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -19,10 +19,131 @@ namespace Calculator
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {   
+        //by default private 
+        double lastNumber, result;
+        SelectedOperator selectedOperator;
         public MainWindow()
         {
             InitializeComponent();
+
+            acButton.Click += AcButton_Click;
+            negativeButton.Click += NegativeButton_Click;
+            percentageButton.Click += PercentageButton_Click;
+            equalButton.Click += EqualButton_Click;
+        }
+
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            double newNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber))
+            {
+                switch(selectedOperator)
+                {
+                    case SelectedOperator.Addition: 
+                       result = SimpleMath.Add(lastNumber ,newNumber) ; 
+                        break;
+                    case SelectedOperator.Division:
+                        result = SimpleMath.Div(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Substraction:
+                        result = SimpleMath.Substract(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.Multiply(lastNumber, newNumber);
+                        break;
+                }
+            }
+            resultLabel.Content = result.ToString();
+        }
+
+        private void PercentageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber / 100;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void NegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(resultLabel.Content.ToString(),out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedValue =int.Parse((sender as Button).Content.ToString());
+
+           
+
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = $"{selectedValue}";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
+            }
+        }
+
+        private void pointButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(!resultLabel.Content.ToString().Contains("."))
+            resultLabel.Content = $"{resultLabel.Content}.";
+        }
+
+        private void OperationButton_Click(object sender, RoutedEvent e)
+        {
+           
+
+                if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+                {
+                    resultLabel.Content = "0";
+                }
+
+            if (sender == multiplyButton) selectedOperator = SelectedOperator.Multiplication;
+            if (sender == minusButton) selectedOperator = SelectedOperator.Substraction;
+            if (sender == divideButton) selectedOperator = SelectedOperator.Division;
+            if (sender == plusButton) selectedOperator = SelectedOperator.Addition;
+            
+
+        }
+    }
+
+    public enum SelectedOperator
+    {
+        Addition,
+        Substraction,
+        Multiplication,
+        Division
+    }
+    public class SimpleMath
+    {
+        public static double  Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public static double Substract(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public static double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        public static double Div(double n1, double n2)
+        {
+            return n1 / n2;
         }
     }
 }
